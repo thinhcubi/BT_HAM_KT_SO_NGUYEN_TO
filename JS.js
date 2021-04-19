f7da3fa430198e707c9e0bd58aa730d28795a108
thine <thinhcubi27@gmail.com>
2021-04-19 14:02:05 +0700

    JS.js

---
+++ JS.js
@@ -0,0 +1,26 @@
+function checkPrime(number) {
+    if (number < 2)
+        return false;
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0)
+            return false;
+
+
+    }return true
+}
+    let count = 0;
+    let number1 = 1;
+
+
+    function displayPrime(num) {
+        while (count < num) {
+            if (checkPrime(number1)) {
+                count++;
+                if(number1<10000)
+                document.write(number1 + '<br>')
+            }
+            number1++;
+        }
+    }
+    displayPrime(6000)
+
